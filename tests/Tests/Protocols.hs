551287aba3d41d772f4bef46776203e9efca1e21
Lara Herzog <lara@qbaylogic.com>
2022-05-24 11:28:11 +0200

    WIP testing

--- tests/Tests/Protocols.hs
+++ tests/Tests/Protocols.hs
@@ -1,13 +1,15 @@
 module Tests.Protocols (tests, main) where
 
-import Prelude
-import Test.Tasty
+import           Prelude
+import           Test.Tasty
 
 import qualified Tests.Protocols.Df
+import qualified Tests.Protocols.Wishbone
 
 tests :: TestTree
 tests = testGroup "Protocols"
   [ Tests.Protocols.Df.tests
+  , Tests.Protocols.Wishbone.tests
   ]
 
 main :: IO ()
