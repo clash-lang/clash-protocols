d5fb1056dea8e7d21b366527c71178358244c9fe
Lara Herzog <lara@qbaylogic.com>
2022-06-02 11:40:05 +0200

    initial pipelined WB circuit validation

--- src/Protocols/Wishbone.hs
+++ src/Protocols/Wishbone.hs
@@ -2,6 +2,7 @@
 Types modelling the Wishbone bus protocol.
 -}
 
+{-# LANGUAGE BlockArguments        #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE GADTs                 #-}
@@ -97,16 +98,24 @@ instance Protocol (Wishbone dom mode addressWidth dat) where
   type Bwd (Wishbone dom mode addressWidth dat) = Signal dom (WishboneS2M dat)
 
 
-instance Backpressure (Wishbone dom mode addressWidth dat) where
-  boolsToBwd _ = C.fromList_lazy . Prelude.map (\b -> wishboneS2M { acknowledge = b })
 
 
-instance (C.KnownNat (C.BitSize dat), C.KnownDomain dom) => Simulate (Wishbone dom mode addressWidth dat) where
-  type SimulateFwdType (Wishbone dom mode addressWidth dat) = [WishboneM2S addressWidth (C.BitSize dat `DivRU` 8) dat]
 
-  type SimulateBwdType (Wishbone dom mode addressWidth dat) = [WishboneS2M dat]
+instance Backpressure (Wishbone dom 'Standard addressWidth dat) where
+  boolsToBwd _ = C.fromList_lazy . Prelude.map (\b ->
+          if b then
+            wishboneS2M { acknowledge = True }
+          else
+            wishboneS2M { err = True }
+        )
 
-  type SimulateChannels (Wishbone dom mode addressWidth dat) = 1
+
+instance (C.KnownNat (C.BitSize dat), C.KnownDomain dom) => Simulate (Wishbone dom 'Standard addressWidth dat) where
+  type SimulateFwdType (Wishbone dom 'Standard addressWidth dat) = [WishboneM2S addressWidth (C.BitSize dat `DivRU` 8) dat]
+
+  type SimulateBwdType (Wishbone dom 'Standard addressWidth dat) = [WishboneS2M dat]
+
+  type SimulateChannels (Wishbone dom 'Standard addressWidth dat) = 1
 
   simToSigFwd Proxy = C.fromList_lazy
   simToSigBwd Proxy = C.fromList_lazy
@@ -128,7 +137,7 @@ instance (C.KnownNat (C.BitSize dat), C.KnownDomain dom) => Simulate (Wishbone d
       toStallReply WishboneM2S{..} s2m ack
         | strobe && busCycle = s2m
         | otherwise = case ack of
-            StallWithNack      -> s2m { acknowledge = False }
+            StallWithNack      -> s2m { err = True }
             StallWithAck       -> s2m { acknowledge = True }
             StallWithErrorX    -> C.errorX "No defined ack"
             StallTransparently -> s2m
@@ -166,15 +175,102 @@ instance (C.KnownNat (C.BitSize dat), C.KnownDomain dom) => Simulate (Wishbone d
 
       go (_sa:sas) (s:ss) _ (f0 :- fwd) ~(b0 :- bwd) =
         let
-          -- Stall a,0s long as s > 0. If s ~ 0, we wait for the RHS to acknowledge
+          -- Stall as long as s > 0. If s ~ 0, we wait for the RHS to acknowledge
           -- the data. As long as RHS does not acknowledge the data, we keep sending
           -- the same data.
           (f1, b1, s1) = case compare 0 s of
-            LT -> (f0 { strobe = False, busCycle = False }, b0 { acknowledge = False }, pred s:ss)    -- s > 0
+            LT -> (f0 { strobe = False, busCycle = False }, b0 { err = True }, pred s:ss)    -- s > 0
             EQ -> (f0, b0, if acknowledge b0 then ss else s:ss) -- s ~ 0
             GT -> error ("Unexpected negative stall: " <> show s) -- s < 0
         in
           B.bimap (b1 :-) (f1 :-) (go sas s1 0 fwd bwd)
+
+
+instance Backpressure (Wishbone dom 'Pipelined addressWidth dat) where
+  boolsToBwd _ = C.fromList_lazy . Prelude.map
+    \b -> if b then wishboneS2M { acknowledge = True } else wishboneS2M { stall = True }
+
+
+instance (C.KnownNat (C.BitSize dat), C.KnownDomain dom) => Simulate (Wishbone dom 'Pipelined addressWidth dat) where
+  type SimulateFwdType (Wishbone dom 'Pipelined addressWidth dat) = [WishboneM2S addressWidth (C.BitSize dat `DivRU` 8) dat]
+
+  type SimulateBwdType (Wishbone dom 'Pipelined addressWidth dat) = [WishboneS2M dat]
+
+  type SimulateChannels (Wishbone dom 'Pipelined addressWidth dat) = 1
+
+  simToSigFwd Proxy = C.fromList_lazy
+  simToSigBwd Proxy = C.fromList_lazy
+  sigToSimFwd Proxy = sample_lazy
+  sigToSimBwd Proxy = sample_lazy
+
+  stallC config (C.head -> (stallAck, stalls)) = Circuit $
+      uncurry (go stallAcks stalls (resetCycles config))
+    where
+      stallAcks
+        | stallAck == StallCycle = [minBound..maxBound] \\ [StallCycle]
+        | otherwise = [stallAck]
+
+      toStallReply
+        :: WishboneM2S addressWidth (C.BitSize dat `DivRU` 8) dat
+        -> WishboneS2M dat
+        -> StallAck
+        -> WishboneS2M dat
+      toStallReply WishboneM2S{..} s2m ack
+        -- not in a bus cycle at all, any slave reply is invalid, but just
+        -- passing through so that potential errors are caught.
+        | C.not busCycle = s2m
+        | busCycle && strobe = s2m
+        | otherwise = case ack of
+            StallWithNack      -> s2m { err = True }
+            StallWithAck       -> s2m { acknowledge = True }
+            StallWithErrorX    -> C.errorX "No defined ack"
+            StallTransparently -> s2m
+            StallCycle         -> C.error "should not happen"
+
+
+      go
+        :: [StallAck]
+        -> [Int]
+        -> Int
+        -> Signal dom (WishboneM2S addressWidth (C.BitSize dat `DivRU` 8) dat)
+        -- ^ input from master
+        -> Signal dom (WishboneS2M dat)
+        -- ^ reply from slave
+        -> ( Signal dom (WishboneS2M dat)
+           , Signal dom (WishboneM2S addressWidth (C.BitSize dat `DivRU` 8) dat))
+      -- "refill" stall acks and continue
+      go [] ss rs fwd bwd = go stallAcks ss rs fwd bwd
+
+      -- resets should be handled by the inputs/driver already, simply pass through
+      go (_:sas) _ resetN (f :- fwd) ~(b :- bwd) | resetN > 0 =
+        B.bimap (b :-) (f :-) (go sas stalls (resetN - 1) fwd bwd)
+
+      -- no more stall cycles to insert
+      go (sa:sas) [] _ (f :- fwd) ~(b :- bwd) =
+        B.bimap (toStallReply f b sa :-) (f :-) (go sas [] 0 fwd bwd)
+
+      go (sa:sas) ss _ (f :- fwd) ~(b :- bwd)
+        | C.not (busCycle f)
+        =
+          -- Left hand side does not send data, simply replicate that behavior. Right
+          -- hand side might send an arbitrary acknowledgement, so we simply pass it
+          -- through.
+
+          -- `f` does not signal/send any data, so it's an "empty" reply.
+          B.bimap (toStallReply f b sa :-) (f :-) (go sas ss 0 fwd bwd)
+
+      go (_sa:sas) (s:ss) _ (f0 :- fwd) ~(b0 :- bwd) =
+        let
+          -- Stall as long as s > 0. If s ~ 0, we wait for the RHS to acknowledge
+          -- the data. As long as RHS does not acknowledge the data, we keep sending
+          -- the same data.
+          (f1, b1, s1) = case compare 0 s of
+            LT -> (f0 { strobe = False }, b0 { stall = True }, pred s:ss)    -- s > 0
+            EQ -> (f0, b0, if acknowledge b0 then ss else s:ss) -- s ~ 0
+            GT -> error ("Unexpected negative stall: " <> show s) -- s < 0
+        in
+          B.bimap (b1 :-) (f1 :-) (go sas s1 0 fwd bwd)
+
 
 
 wishboneM2S :: (C.KnownNat addressWidth, C.KnownNat (C.BitSize dat)) => WishboneM2S addressWidth (C.BitSize dat `DivRU` 8) dat
