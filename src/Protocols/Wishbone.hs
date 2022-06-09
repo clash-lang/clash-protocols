2394188321d1d46bebfb198526222ee72ea91c5b
Lara Herzog <lara@qbaylogic.com>
2022-06-09 15:32:27 +0200

    wishbone pipelined driver and stalling circuit, tests time out :(

--- src/Protocols/Wishbone.hs
+++ src/Protocols/Wishbone.hs
@@ -19,6 +19,7 @@ import qualified Clash.Prelude         as C
 import           Clash.Signal.Internal (Signal (..), sample_lazy)
 
 import           Control.DeepSeq       (NFData)
+import           Control.Monad.State
 import qualified Data.Bifunctor        as B
 import           Data.Data             (Proxy (Proxy))
 import           Data.List             ((\\))
@@ -203,73 +204,8 @@ instance (C.KnownNat (C.BitSize dat), C.KnownDomain dom) => Simulate (Wishbone d
   sigToSimFwd Proxy = sample_lazy
   sigToSimBwd Proxy = sample_lazy
 
-  stallC config (C.head -> (stallAck, stalls)) = Circuit $
-      uncurry (go stallAcks stalls (resetCycles config))
-    where
-      stallAcks
-        | stallAck == StallCycle = [minBound..maxBound] \\ [StallCycle]
-        | otherwise = [stallAck]
+  stallC = C.undefined
 
-      toStallReply
-        :: WishboneM2S addressWidth (C.BitSize dat `DivRU` 8) dat
-        -> WishboneS2M dat
-        -> StallAck
-        -> WishboneS2M dat
-      toStallReply WishboneM2S{..} s2m ack
-        -- not in a bus cycle at all, any slave reply is invalid, but just
-        -- passing through so that potential errors are caught.
-        | C.not busCycle = s2m
-        | busCycle && strobe = s2m
-        | otherwise = case ack of
-            StallWithNack      -> s2m { err = True }
-            StallWithAck       -> s2m { acknowledge = True }
-            StallWithErrorX    -> C.errorX "No defined ack"
-            StallTransparently -> s2m
-            StallCycle         -> C.error "should not happen"
-
-
-      go
-        :: [StallAck]
-        -> [Int]
-        -> Int
-        -> Signal dom (WishboneM2S addressWidth (C.BitSize dat `DivRU` 8) dat)
-        -- ^ input from master
-        -> Signal dom (WishboneS2M dat)
-        -- ^ reply from slave
-        -> ( Signal dom (WishboneS2M dat)
-           , Signal dom (WishboneM2S addressWidth (C.BitSize dat `DivRU` 8) dat))
-      -- "refill" stall acks and continue
-      go [] ss rs fwd bwd = go stallAcks ss rs fwd bwd
-
-      -- resets should be handled by the inputs/driver already, simply pass through
-      go (_:sas) _ resetN (f :- fwd) ~(b :- bwd) | resetN > 0 =
-        B.bimap (b :-) (f :-) (go sas stalls (resetN - 1) fwd bwd)
-
-      -- no more stall cycles to insert
-      go (sa:sas) [] _ (f :- fwd) ~(b :- bwd) =
-        B.bimap (toStallReply f b sa :-) (f :-) (go sas [] 0 fwd bwd)
-
-      go (sa:sas) ss _ (f :- fwd) ~(b :- bwd)
-        | C.not (busCycle f)
-        =
-          -- Left hand side does not send data, simply replicate that behavior. Right
-          -- hand side might send an arbitrary acknowledgement, so we simply pass it
-          -- through.
-
-          -- `f` does not signal/send any data, so it's an "empty" reply.
-          B.bimap (toStallReply f b sa :-) (f :-) (go sas ss 0 fwd bwd)
-
-      go (_sa:sas) (s:ss) _ (f0 :- fwd) ~(b0 :- bwd) =
-        let
-          -- Stall as long as s > 0. If s ~ 0, we wait for the RHS to acknowledge
-          -- the data. As long as RHS does not acknowledge the data, we keep sending
-          -- the same data.
-          (f1, b1, s1) = case compare 0 s of
-            LT -> (f0 { strobe = False }, b0 { stall = True }, pred s:ss)    -- s > 0
-            EQ -> (f0, b0, if acknowledge b0 then ss else s:ss) -- s ~ 0
-            GT -> error ("Unexpected negative stall: " <> show s) -- s < 0
-        in
-          B.bimap (b1 :-) (f1 :-) (go sas s1 0 fwd bwd)
 
 
 
@@ -295,3 +231,6 @@ wishboneS2M
   , retry = False
   , stall = False
   }
+
+terminationSignal :: WishboneS2M dat -> Bool
+terminationSignal s2m = acknowledge s2m || err s2m || retry s2m
--- tests/Tests/Protocols/Wishbone.hs
+++ tests/Tests/Protocols/Wishbone.hs
@@ -15,7 +15,8 @@ import           Hedgehog.Gen                   as Gen
 import           Hedgehog.Range                 as Range
 
 import           Protocols
-import           Protocols.Hedgehog             (defExpectOptions)
+import           Protocols.Hedgehog             (ExpectOptions (eoTimeout),
+                                                 defExpectOptions)
 import           Protocols.Wishbone
 import           Protocols.Wishbone.Hedgehog
 
@@ -24,10 +25,6 @@ import           Test.Tasty
 import           Test.Tasty.Hedgehog            (HedgehogTestLimit (HedgehogTestLimit))
 import           Test.Tasty.Hedgehog.Extra      (testProperty)
 import           Test.Tasty.TH                  (testGroupGenerator)
-
-data Transfer addrWidth dat
-  = Read (BitVector addrWidth)
-  | Write (BitVector addrWidth) dat
 
 smallInt :: Range Int
 smallInt = Range.linear 0 10
@@ -40,29 +37,19 @@ genData genA = do
   n <- genSmallInt
   Gen.list (Range.singleton n) genA
 
-genWishboneTransfer :: (KnownNat addressWidth) => Gen a -> Gen (Transfer addressWidth a)
+genWishboneTransfer :: (KnownNat addressWidth) => Gen a -> Gen (WishboneMasterRequest addressWidth a)
 genWishboneTransfer genA = do
   Gen.choice [Read <$> genBitVector, Write <$> genBitVector <*> genA]
 
 transfersToSignalsStandard
   :: forall addressWidth a . (KnownNat addressWidth, KnownNat (BitSize a))
-  => [Transfer addressWidth a]
+  => [WishboneMasterRequest addressWidth a]
   -> [WishboneM2S addressWidth (BitSize a `DivRU` 8) a]
 transfersToSignalsStandard = Prelude.concatMap $ \case
    Read bv -> [ (wishboneM2S @addressWidth @a) { strobe = False, busCycle = False }
               , (wishboneM2S @addressWidth @a) { strobe = True, busCycle = True, addr = bv, writeEnable = False }]
    Write bv a -> [ wishboneM2S
                  , (wishboneM2S @addressWidth @a) { strobe = True, busCycle = True, addr = bv, writeEnable = True, writeData = a }]
-
-transfersToSignalsPipelined
-  :: forall addressWidth a . (KnownNat addressWidth, KnownNat (BitSize a))
-  => [[Transfer addressWidth a]]
-  -> [WishboneM2S addressWidth (BitSize a `DivRU` 8) a]
-transfersToSignalsPipelined ts = Prelude.concat $ flip Prelude.map ts $ \t ->
-    (wishboneM2S @addressWidth @a) { strobe = False, busCycle = False } : Prelude.map transferToSignal t
-  where
-    transferToSignal (Read bv)    = (wishboneM2S @addressWidth @a) { strobe = True, busCycle = True, addr = bv, writeEnable = False }
-    transferToSignal (Write bv a) = (wishboneM2S @addressWidth @a) { strobe = True, busCycle = True, addr = bv, writeEnable = True, writeData = a }
 
 
 idWriteWbSt :: (BitPack a) => Circuit (Wishbone dom 'Standard selWidth a) ()
@@ -88,15 +75,15 @@ idWriteWbPip = Circuit go
 
 
 prop_idWriteSt :: Property
-prop_idWriteSt = validateStallingStandardCircuit @System defExpectOptions genDat idWriteWbSt
+prop_idWriteSt =  validateStallingStandardCircuit @System defExpectOptions genDat idWriteWbSt
   where
     genDat = transfersToSignalsStandard <$> genData (genWishboneTransfer @10 genSmallInt)
 
 
 prop_idWritePip :: Property
-prop_idWritePip = validateStallingPipelineCircuit @System defExpectOptions genDat idWriteWbPip
+prop_idWritePip = withClockResetEnable @System clockGen resetGen enableGen $ validateStallingPipelineCircuit @System (defExpectOptions { eoTimeout = Just 100 }) genDat idWriteWbPip
   where
-    genDat = transfersToSignalsPipelined <$> genData (genData (genWishboneTransfer @10 genSmallInt))
+    genDat = genData (genData (genWishboneTransfer @10 genSmallInt))
 
 
 tests :: TestTree
@@ -104,5 +91,5 @@ tests =
     -- TODO: Move timeout option to hedgehog for better error messages.
     -- TODO: Does not seem to work for combinatorial loops like @let x = x in x@??
     localOption (mkTimeout 12_000_000 {- 12 seconds -})
-  $ localOption (HedgehogTestLimit (Just 1000))
+  $ localOption (HedgehogTestLimit (Just 12))
   $(testGroupGenerator)
