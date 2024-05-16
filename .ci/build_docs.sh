86ce4b93c049ba019534d25c136b0bf9ed945a57
Lucas Bollen <lucas@qbaylogic.com>
2024-05-16 14:47:33 +0200

    Suppress Haddock re-export warning

--- .ci/build_docs.sh
+++ .ci/build_docs.sh
@@ -20,29 +20,36 @@ cabal haddock \
   |& tee haddock_log
 
 set +e
-if grep -q "Missing documentation" haddock_log; then
+
+suppressed_warnings=(
+  'Consider exporting it together with its parent(s) for code clarity.'
+  )
+
+grep -v -e "${suppressed_warnings[@]}" haddock_log |& tee haddock_filtered
+
+if grep -q "Missing documentation" haddock_filtered; then
   echo -e "\e[1m\e[31mMissing documentation! Scroll up for full log.\e[0m"
-  grep --color=always -n -C 5 "Missing documentation" haddock_log
+  grep --color=always -n -C 5 "Missing documentation" haddock_filtered
   exit 1
 fi
 
-if grep -q "If you qualify the identifier, haddock can try to link it anyway" haddock_log; then
+if grep -q "If you qualify the identifier, haddock can try to link it anyway" haddock_filtered; then
   echo -e "\e[1m\e[31mIdentifier out of scope! Scroll up for full log.\e[0m"
-  grep --color=always -n -C 5 "If you qualify the identifier, haddock can try to link it anyway" haddock_log
+  grep --color=always -n -C 5 "If you qualify the identifier, haddock can try to link it anyway" haddock_filtered
   exit 1
 fi
 
-if grep -q "could not find link destinations for" haddock_log; then
+if grep -q "could not find link destinations for" haddock_filtered; then
   echo -e "\e[1m\e[31mAn identifier could not be linked! Scroll up for full log.\e[0m"
-  grep --color=always -n -C 5 "could not find link destinations for" haddock_log
+  grep --color=always -n -C 5 "could not find link destinations for" haddock_filtered
   exit 1
 fi
 
-if grep -E -q "^Warning:" haddock_log; then
+if grep -E -q "^Warning:" haddock_filtered; then
   echo -e "\e[1m\e[31mAn unknown warning occured. Scroll up for full log.\e[0m"
-  grep --color=always -n -C 5 -E "^Warning:" haddock_log
+  grep --color=always -n -C 5 -E "^Warning:" haddock_filtered
   exit 1
 fi
 
 # Copy documention to docs/
-ln -s "$(dirname "$(tail -n1 haddock_log)")" docs
+ln -s "$(dirname "$(tail -n1 haddock_filtered)")" docs
