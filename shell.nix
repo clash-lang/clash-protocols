f38e86d9ab3b1493f1425ba29220f00cd954b319
Rowan Goemans <goemansrowan@gmail.com>
2024-06-10 17:21:38 +0200

    Nix: Add skeleton

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ nixpkgs ? import ./nix/nixpkgs.nix {} }:
+let
+  inherit (nixpkgs) pkgs;
+  inherit (pkgs) haskellPackages;
+
+  project = import ./release.nix;
+in
+
+pkgs.stdenv.mkDerivation {
+  name = "shell";
+  buildInputs = project.env.nativeBuildInputs ++ [
+    haskellPackages.cabal-install
+    haskellPackages.fourmolu
+  ];
+  LC_ALL = "C.UTF-8";
+}
