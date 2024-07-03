63f78a98d82cfbbd78302d3fb726e294b8e1847b
Rowan Goemans <goemansrowan@gmail.com>
2024-07-03 15:56:17 +0200

    Add PacketStream protocol.
    Co-authored-by: Jasmijn Bookelmann <bookelmannjasmijn@gmail.com>
    Co-authored-by: Cato van Ojen <cato.vanojen@student.ru.nl>>
    Co-authored-by: MatthijsMu <93450301+MatthijsMu@users.noreply.github.com>
    Co-authored-by: t-wallet <tw.personal@tutanota.com>
    Co-authored-by: Jasper Laumen <96011116+JLaumen@users.noreply.github.com>
    Co-authored-by: Mart Koster <30956441+Akribes@users.noreply.github.com>
    Co-authored-by: Bryan Rinders <bryan__ajax@hotmail.com>
    Co-authored-by: Daan Weessies <daan.weessies@gmail.com>

---
+++ clash-protocols/tests/Tests/Protocols/PacketStream/AsyncFifo.hs
@@ -0,0 +1,127 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Tests.Protocols.PacketStream.AsyncFifo where
+
+-- base
+import Prelude
+
+-- clash-prelude
+import Clash.Prelude as C
+
+-- hedgehog
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
+-- tasty
+import Test.Tasty
+import Test.Tasty.Hedgehog (HedgehogTestLimit (HedgehogTestLimit))
+import Test.Tasty.Hedgehog.Extra (testProperty)
+import Test.Tasty.TH (testGroupGenerator)
+
+-- clash-protocols
+import Protocols
+import Protocols.Hedgehog
+import Protocols.PacketStream.AsyncFifo
+import Protocols.PacketStream.Base
+
+genVec :: (KnownNat n, 1 C.<= n) => Gen a -> Gen (Vec n a)
+genVec gen = sequence (C.repeat gen)
+
+createDomain
+  vSystem
+    { vName = "TestDom50"
+    , vPeriod = 20_000
+    , vActiveEdge = Rising
+    , vResetKind = Asynchronous
+    , vInitBehavior = Unknown
+    , vResetPolarity = ActiveHigh
+    }
+
+createDomain
+  vSystem
+    { vName = "TestDom125"
+    , vPeriod = 8_000
+    , vActiveEdge = Rising
+    , vResetKind = Asynchronous
+    , vInitBehavior = Unknown
+    , vResetPolarity = ActiveHigh
+    }
+
+clk50 :: Clock TestDom50
+clk50 = clockGen
+
+clk125 :: Clock TestDom125
+clk125 = clockGen
+
+rst50 :: Reset TestDom50
+rst50 = resetGen @TestDom50
+
+rst125 :: Reset TestDom125
+rst125 = resetGen @TestDom125
+
+en50 :: Enable TestDom50
+en50 = enableGen
+
+en125 :: Enable TestDom125
+en125 = enableGen
+
+generateAsyncFifoIdProp ::
+  forall (wDom :: Domain) (rDom :: Domain).
+  (KnownDomain wDom, KnownDomain rDom) =>
+  Clock wDom ->
+  Reset wDom ->
+  Enable wDom ->
+  Clock rDom ->
+  Reset rDom ->
+  Enable rDom ->
+  Property
+generateAsyncFifoIdProp wClk wRst wEn rClk rRst rEn =
+  propWithModel
+    defExpectOptions
+    (Gen.list (Range.linear 0 100) genPackets)
+    id
+    ckt
+    (===)
+ where
+  ckt ::
+    (KnownDomain wDom, KnownDomain rDom) =>
+    Circuit
+      (PacketStream wDom 1 Int)
+      (PacketStream rDom 1 Int)
+  ckt = asyncFifoC (C.SNat @8) wClk wRst wEn rClk rRst rEn
+  -- This is used to generate
+  genPackets =
+    PacketStreamM2S
+      <$> genVec Gen.enumBounded
+      <*> Gen.maybe Gen.enumBounded
+      <*> Gen.enumBounded
+      <*> Gen.enumBounded
+
+{- | The async FIFO circuit should forward all of its input data without loss and without producing extra data.
+  This property tests whether this is true, when the clock of the writer and reader is equally fast (50 MHz).
+-}
+prop_asyncfifo_writer_speed_equal_to_reader_id :: Property
+prop_asyncfifo_writer_speed_equal_to_reader_id = generateAsyncFifoIdProp clk50 rst50 en50 clk50 rst50 en50
+
+{- | The async FIFO circuit should forward all of its input data without loss and without producing extra data.
+  This property tests whether this is true, when the clock of the writer (50 MHz) is slower than the clock of the reader (125 MHz).
+-}
+prop_asyncfifo_writer_speed_slower_than_reader_id :: Property
+prop_asyncfifo_writer_speed_slower_than_reader_id = generateAsyncFifoIdProp clk50 rst50 en50 clk125 rst125 en125
+
+{- | The async FIFO circuit should forward all of its input data without loss and without producing extra data.
+  This property tests whether this is true, when the clock of the writer (125 MHz) is faster than the clock of the reader (50 MHz).
+-}
+prop_asyncfifo_writer_speed_faster_than_reader_id :: Property
+prop_asyncfifo_writer_speed_faster_than_reader_id = generateAsyncFifoIdProp clk125 rst125 en125 clk50 rst50 en50
+
+tests :: TestTree
+tests =
+  localOption (mkTimeout 12_000_000 {- 12 seconds -}) $
+    localOption
+      (HedgehogTestLimit (Just 1_000))
+      $(testGroupGenerator)
---
+++ clash-protocols/tests/Tests/Protocols/PacketStream/Routing.hs
@@ -0,0 +1,159 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Tests.Protocols.PacketStream.Routing where
+
+-- base
+import Data.List (groupBy, sortOn)
+import Prelude
+
+-- clash-prelude
+import Clash.Prelude (type (<=))
+import qualified Clash.Prelude as C
+
+-- hedgehog
+import Hedgehog hiding (Parallel)
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+
+-- tasty
+import Test.Tasty
+import Test.Tasty.Hedgehog (HedgehogTestLimit (HedgehogTestLimit))
+import Test.Tasty.Hedgehog.Extra (testProperty)
+import Test.Tasty.TH (testGroupGenerator)
+
+-- clash-protocols
+
+import Protocols.Hedgehog
+import Protocols.PacketStream.Base
+import Protocols.PacketStream.Routing
+
+-- tests
+import Tests.Protocols.PacketStream.Base (chunkByPacket, fullPackets)
+
+genVec :: (C.KnownNat n, 1 <= n) => Gen a -> Gen (C.Vec n a)
+genVec gen = sequence (C.repeat gen)
+
+-- | Tests the round-robin packet arbiter with one source; essentially an id test
+prop_packetarbiter_roundrobin_id :: Property
+prop_packetarbiter_roundrobin_id = makePropPacketArbiter C.d1 C.d2 RoundRobin
+
+-- | Tests the parallel packet arbiter with one source; essentially an id test
+prop_packetarbiter_parallel_id :: Property
+prop_packetarbiter_parallel_id = makePropPacketArbiter C.d1 C.d2 Parallel
+
+-- Tests the round-robin arbiter with five sources
+prop_packetarbiter_roundrobin :: Property
+prop_packetarbiter_roundrobin = makePropPacketArbiter C.d5 C.d2 RoundRobin
+
+-- Tests the parallel arbiter with five sources
+prop_packetarbiter_parallel :: Property
+prop_packetarbiter_parallel = makePropPacketArbiter C.d5 C.d2 Parallel
+
+{- | Tests a packet arbiter for any data width and number of sources. In particular,
+tests that packets from all sources are sent out unmodified in the same order
+they were in in the source streams.
+-}
+makePropPacketArbiter ::
+  forall p n.
+  ( C.KnownNat p
+  , 1 <= p
+  , C.KnownNat n
+  , 1 <= n
+  ) =>
+  C.SNat p ->
+  C.SNat n ->
+  ArbiterMode ->
+  Property
+makePropPacketArbiter _ _ mode =
+  propWithModelSingleDomain
+    @C.System
+    defExpectOptions
+    genSources
+    (C.exposeClockResetEnable concat)
+    (C.exposeClockResetEnable (packetArbiterC mode))
+    (\xs ys -> partitionPackets xs === partitionPackets ys)
+ where
+  genSources = mapM (fmap fullPackets . Gen.list (Range.linear 0 100) . genPacket) (C.indicesI @p)
+  -- TODO use util function from client review branch
+  genPacket i =
+    PacketStreamM2S
+      <$> genVec @n Gen.enumBounded
+      <*> Gen.maybe Gen.enumBounded
+      <*> pure i
+      <*> Gen.enumBounded
+
+  partitionPackets packets =
+    sortOn (_meta . head . head) $
+      groupBy (\a b -> _meta a == _meta b) <$> chunkByPacket packets
+
+{- | Tests that the packet dispatcher works correctly with one sink that accepts
+all packets; essentially an id test.
+-}
+prop_packetdispatcher_id :: Property
+prop_packetdispatcher_id =
+  makePropPacketDispatcher
+    C.d4
+    ((const True :: Int -> Bool) C.:> C.Nil)
+
+{- | Tests the packet dispatcher for a data width of four bytes and three
+overlapping but incomplete dispatch functions, effectively testing whether
+the circuit sends input to the first allowed output channel and drops input
+if there are none.
+-}
+prop_packetdispatcher :: Property
+prop_packetdispatcher = makePropPacketDispatcher C.d4 fs
+ where
+  fs :: C.Vec 3 (C.Index 4 -> Bool)
+  fs =
+    (>= 3)
+      C.:> (>= 2)
+      C.:> (>= 1)
+      C.:> C.Nil
+
+{- | Generic test function for the packet dispatcher, testing for all data widths,
+dispatch functions, and some meta types
+-}
+makePropPacketDispatcher ::
+  forall (p :: C.Nat) (dataWidth :: C.Nat) (a :: C.Type).
+  ( C.KnownNat p
+  , 1 <= p
+  , C.KnownNat dataWidth
+  , 1 <= dataWidth
+  , TestType a
+  , Bounded a
+  , Enum a
+  ) =>
+  C.SNat dataWidth ->
+  C.Vec p (a -> Bool) ->
+  Property
+makePropPacketDispatcher _ fs =
+  idWithModelSingleDomain @C.System
+    defExpectOptions
+    (Gen.list (Range.linear 0 100) genPackets)
+    (C.exposeClockResetEnable (model 0))
+    (C.exposeClockResetEnable (packetDispatcherC fs))
+ where
+  model ::
+    C.Index p -> [PacketStreamM2S dataWidth a] -> C.Vec p [PacketStreamM2S dataWidth a]
+  model _ [] = pure []
+  model i (y : ys)
+    | (fs C.!! i) (_meta y) = let next = model 0 ys in C.replace i (y : (next C.!! i)) next
+    | i < maxBound = model (i + 1) (y : ys)
+    | otherwise = model 0 ys
+
+  -- TODO use util function from client review branch
+  genPackets =
+    PacketStreamM2S
+      <$> genVec @dataWidth Gen.enumBounded
+      <*> Gen.maybe Gen.enumBounded
+      <*> Gen.enumBounded
+      <*> Gen.enumBounded
+
+tests :: TestTree
+tests =
+  localOption (mkTimeout 12_000_000 {- 12 seconds -}) $
+    localOption
+      (HedgehogTestLimit (Just 1_000))
+      $(testGroupGenerator)
