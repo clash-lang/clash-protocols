6c1f3491864afa6d3057af037093b86e6fe2e609
t-wallet <tw.personal@tutanota.com>
2024-07-22 15:55:03 +0200

    re-add tests

--- clash-protocols/tests/Tests/Protocols.hs
+++ clash-protocols/tests/Tests/Protocols.hs
@@ -13,13 +13,13 @@ tests :: TestTree
 tests =
   testGroup
     "Protocols"
-    [ --Tests.Protocols.Df.tests
-    --, Tests.Protocols.DfConv.tests
-    --, Tests.Protocols.Avalon.tests
-    --, Tests.Protocols.Axi4.tests
-     Tests.Protocols.PacketStream.tests
-    --, Tests.Protocols.Wishbone.tests
-    --, Tests.Protocols.Vec.tests
+    [ Tests.Protocols.Df.tests
+    , Tests.Protocols.DfConv.tests
+    , Tests.Protocols.Avalon.tests
+    , Tests.Protocols.Axi4.tests
+    , Tests.Protocols.PacketStream.tests
+    , Tests.Protocols.Wishbone.tests
+    , Tests.Protocols.Vec.tests
     ]
 
 main :: IO ()
