0b486f405cc791662e8f1e3fbbf78a0ea12f248e
t-wallet <tw.personal@tutanota.com>
2024-07-26 16:22:31 +0200

    Optimize packetizer

--- clash-protocols/src/Protocols/PacketStream/Packetizers.hs
+++ clash-protocols/src/Protocols/PacketStream/Packetizers.hs
@@ -17,155 +17,273 @@ import Protocols
 import qualified Protocols.Df as Df
 import Protocols.PacketStream.Base
 
+import Clash.Sized.Vector.Extra (takeLe)
+import Data.Data ((:~:) (Refl))
 import Data.Maybe
 import Data.Maybe.Extra
 
 defaultByte :: BitVector 8
 defaultByte = 0x00
 
-type HeaderBufSize (headerBytes :: Nat) (dataWidth :: Nat) =
-  headerBytes + dataWidth
-
--- The amount of bytes that we still need to forward due to
--- @headerBytes@ not aligning with @dataWidth@.
-type ForwardBufSize (headerBytes :: Nat) (dataWidth :: Nat) =
-  headerBytes `Mod` dataWidth
-
-type PacketizerCt (headerBytes :: Nat) (dataWidth :: Nat) =
-  ( KnownNat dataWidth
-  , 1 <= dataWidth
+type PacketizerCt (header :: Type) (headerBytes :: Nat) (dataWidth :: Nat) =
+  ( BitPack header
+  , BitSize header ~ headerBytes * 8
   , KnownNat headerBytes
+  , KnownNat dataWidth
+  , 1 <= headerBytes
+  , 1 <= dataWidth
   )
 
-data PacketizerState (metaOut :: Type) (headerBytes :: Nat) (dataWidth :: Nat)
-  = Insert
-      { _counter :: Index (headerBytes `DivRU` dataWidth)
-      , _hdrBuf :: Vec (HeaderBufSize headerBytes dataWidth) (BitVector 8)
-      , _aborted :: Bool
+data PacketizerState1 (metaOut :: Type) (headerBytes :: Nat) (dataWidth :: Nat)
+  = Insert1
+      { _aborted1 :: Bool
       }
-  | Forward
-      { _fwdBuf :: Vec (ForwardBufSize headerBytes dataWidth) (BitVector 8)
-      , _aborted :: Bool
+  | Forward1
+      { _aborted1 :: Bool
+      , _hdrBuf1 :: Vec headerBytes (BitVector 8)
       }
-  | LastForward
-      {_lastFragment :: PacketStreamM2S dataWidth metaOut}
-  deriving (Generic, Show, ShowX)
+  | LastForward1
+      { _aborted1 :: Bool
+      , _hdrBuf1 :: Vec headerBytes (BitVector 8)
+      }
+  deriving (Generic, NFDataX, Show, ShowX)
 
-deriving instance
-  (NFDataX metaOut, PacketizerCt headerBytes dataWidth) =>
-  NFDataX (PacketizerState metaOut headerBytes dataWidth)
-
--- | Initial state of @packetizerT@
-instance
-  (PacketizerCt headerBytes dataWidth) =>
-  Default (PacketizerState metaOut headerBytes dataWidth)
-  where
-  def :: PacketizerState metaOut headerBytes dataWidth
-  def = Insert 0 (repeat defaultByte) False
-
-adjustLast ::
-  forall
-    (headerBytes :: Nat)
-    (dataWidth :: Nat).
-  ( headerBytes `Mod` dataWidth <= dataWidth
-  , KnownNat dataWidth
-  , 1 <= dataWidth
-  ) =>
-  SNat headerBytes ->
-  Index dataWidth ->
-  Either (Index dataWidth) (Index dataWidth)
-adjustLast SNat idx = if outputNow then Left nowIdx else Right nextIdx
- where
-  outputNow = case compareSNat (SNat @(ForwardBufSize headerBytes dataWidth)) d0 of
-    SNatGT -> idx < natToNum @(dataWidth - ForwardBufSize headerBytes dataWidth)
-    _ -> True
-  nowIdx = idx + natToNum @(ForwardBufSize headerBytes dataWidth)
-  nextIdx = idx - natToNum @(dataWidth - ForwardBufSize headerBytes dataWidth)
-
-packetizerT ::
+-- | Packetizer transition function in case @dataWidth > headerBytes@
+packetizerT1 ::
   forall
     (headerBytes :: Nat)
     (dataWidth :: Nat)
     (header :: Type)
     (metaIn :: Type)
     (metaOut :: Type).
-  (BitSize header ~ headerBytes * 8) =>
-  (BitPack header) =>
-  (PacketizerCt headerBytes dataWidth) =>
-  (ForwardBufSize headerBytes dataWidth <= dataWidth) =>
+  (PacketizerCt header headerBytes dataWidth) =>
+  (headerBytes + 1 <= dataWidth) =>
   (metaIn -> metaOut) ->
   (metaIn -> header) ->
-  PacketizerState metaOut headerBytes dataWidth ->
+  PacketizerState1 metaOut headerBytes dataWidth ->
   (Maybe (PacketStreamM2S dataWidth metaIn), PacketStreamS2M) ->
-  ( PacketizerState metaOut headerBytes dataWidth
+  ( PacketizerState1 metaOut headerBytes dataWidth
   , (PacketStreamS2M, Maybe (PacketStreamM2S dataWidth metaOut))
   )
-packetizerT toMetaOut toHeader st@Insert{..} (Just pkt@PacketStreamM2S{..}, bwdIn) =
-  (nextStOut, (bwdOut, fwdOut))
+packetizerT1 toMetaOut toHeader st (Just inPkt, bwdIn) =
+  let
+    go buf = (nextStOut, (bwdOut, Just outPkt))
+     where
+      bytesOut :: Vec (dataWidth - headerBytes) (BitVector 8)
+      (bytesOut, newBuf) =
+        leToPlus @headerBytes @dataWidth $ splitAt (SNat @(dataWidth - headerBytes)) (_data inPkt)
+      nextAborted = _aborted1 st || _abort inPkt
+
+      outPkt =
+        inPkt
+          { _data = buf ++ bytesOut
+          , _last = newLast
+          , _meta = toMetaOut (_meta inPkt)
+          , _abort = nextAborted
+          }
+
+      (nextSt, newLast) = case _last inPkt of
+        Nothing -> (Forward1 nextAborted newBuf, Nothing)
+        Just i
+          | i < natToNum @(dataWidth - headerBytes) ->
+              (Insert1 False, Just (i + natToNum @headerBytes))
+          | otherwise -> (LastForward1 nextAborted newBuf, Nothing)
+
+      nextStOut = if _ready bwdIn then nextSt else st
+      bwdOut = case nextStOut of
+        LastForward1{} -> PacketStreamS2M False
+        _ -> bwdIn
+   in
+    case st of
+      Insert1{} -> go (bitCoerce (toHeader (_meta inPkt)))
+      Forward1{..} -> go _hdrBuf1
+      LastForward1{..} -> (nextStOut, (bwdIn, Just outPkt))
+       where
+        outPkt =
+          inPkt
+            { _data = _hdrBuf1 ++ repeat defaultByte
+            , _last = (\i -> i - natToNum @(dataWidth - headerBytes)) <$> _last inPkt
+            , _meta = toMetaOut (_meta inPkt)
+            , _abort = _aborted1 || _abort inPkt
+            }
+        nextStOut = if _ready bwdIn then Insert1 False else st
+packetizerT1 _ _ s (Nothing, bwdIn) = (s, (bwdIn, Nothing))
+
+data PacketizerState2 (metaOut :: Type) (headerBytes :: Nat) (dataWidth :: Nat)
+  = LoadHeader2
+  | Insert2
+      { _aborted2 :: Bool
+      , _hdrBuf2 :: Vec headerBytes (BitVector 8)
+      , _counter2 :: Index (headerBytes `DivRU` dataWidth)
+      }
+  | Forward2
+      { _aborted2 :: Bool
+      }
+  deriving (Generic, NFDataX, Show, ShowX)
+
+-- | Packetizer transition function in case @dataWidth <= headerBytes@ and @headerBytes % dataWidth ~ 0@
+packetizerT2 ::
+  forall
+    (headerBytes :: Nat)
+    (dataWidth :: Nat)
+    (header :: Type)
+    (metaIn :: Type)
+    (metaOut :: Type).
+  (PacketizerCt header headerBytes dataWidth) =>
+  (headerBytes `Mod` dataWidth ~ 0) =>
+  (dataWidth <= headerBytes) =>
+  (metaIn -> metaOut) ->
+  (metaIn -> header) ->
+  PacketizerState2 metaOut headerBytes dataWidth ->
+  (Maybe (PacketStreamM2S dataWidth metaIn), PacketStreamS2M) ->
+  ( PacketizerState2 metaOut headerBytes dataWidth
+  , (PacketStreamS2M, Maybe (PacketStreamM2S dataWidth metaOut))
+  )
+-- Load the metadata into a buffer. This costs one extra cycle of latency, but it reduces resource usage.
+packetizerT2 _ toHeader LoadHeader2 (Just inPkt, _) =
+  (Insert2 False (bitCoerce (toHeader (_meta inPkt))) 0, (PacketStreamS2M False, Nothing))
+packetizerT2 toMetaOut _ st@Insert2{..} (Just inPkt, bwdIn) =
+  (nextStOut, (PacketStreamS2M False, Just outPkt))
  where
-  alignedCmp = compareSNat (SNat @(ForwardBufSize headerBytes dataWidth)) d0
-  nextAborted = _aborted || _abort
-  header = bitCoerce (toHeader _meta)
-  metaOut = toMetaOut _meta
-  hdrBuf = if _counter == 0 then header ++ _data else _hdrBuf
-  (newHdrBuf, dataOut) = shiftOutFrom0 (SNat @dataWidth) hdrBuf
-  forwardBytes = snd $ shiftOutFromN (SNat @(ForwardBufSize headerBytes dataWidth)) _data
-
-  newLast = case alignedCmp of
-    SNatGT -> fmap (adjustLast (SNat @headerBytes)) _last
-    _ -> Nothing
-
-  fwdOut =
-    Just
-      pkt
-        { _data = dataOut
-        , _last = if _counter == maxBound then either Just (const Nothing) =<< newLast else Nothing
-        , _meta = metaOut
-        , _abort = nextAborted
-        }
-
-  nextSt = case (_counter == maxBound, newLast) of
-    (False, _) -> Insert (succ _counter) newHdrBuf nextAborted
-    (True, Nothing) -> Forward forwardBytes nextAborted
-    (True, Just (Left _)) -> def
-    (True, Just (Right idx)) ->
-      LastForward
-        (PacketStreamM2S (take (SNat @dataWidth) newHdrBuf) (Just idx) metaOut nextAborted)
-
-  nextStOut = if isNothing fwdOut || _ready bwdIn then nextSt else st
-
-  -- Assert backpressure while inserting the header. If shifting needs to be done
-  -- and we are at the last cycle of insertion, we do not need to assert backpressure
-  -- because we put the rest of the data in _fwdBuf (of course, unless our subordinate asserts backpressure).
-  bwdOut = PacketStreamS2M $ case alignedCmp of
-    SNatGT -> _ready bwdIn && _counter == maxBound
-    _ -> False
-packetizerT toMetaOut _ st@Forward{..} (Just pkt@PacketStreamM2S{..}, bwdIn) = (nextStOut, (bwdIn, Just outPkt))
- where
-  nextAborted = _aborted || _abort
-  metaOut = toMetaOut _meta
-  (dataOut, nextFwdBuf) = splitAt (SNat @dataWidth) (_fwdBuf ++ _data)
-  dataLast = nextFwdBuf ++ repeat @(dataWidth - ForwardBufSize headerBytes dataWidth) defaultByte
-  newLast = fmap (adjustLast (SNat @headerBytes)) _last
-
+  (newBuf, dataOut) = leToPlus @dataWidth @headerBytes $ shiftOutFrom0 (SNat @dataWidth) _hdrBuf2
+  nextAborted = _aborted2 || _abort inPkt
   outPkt =
-    pkt
+    inPkt
       { _data = dataOut
-      , _last = either Just (const Nothing) =<< newLast
-      , _meta = metaOut
+      , _last = Nothing
+      , _meta = toMetaOut (_meta inPkt)
       , _abort = nextAborted
       }
 
-  nextSt = case newLast of
-    Nothing -> Forward nextFwdBuf nextAborted
-    Just (Left _) -> def
-    Just (Right idx) -> LastForward (PacketStreamM2S dataLast (Just idx) metaOut nextAborted)
+  nextSt
+    | _counter2 == maxBound = Forward2 nextAborted
+    | otherwise = Insert2 nextAborted newBuf (succ _counter2)
 
   nextStOut = if _ready bwdIn then nextSt else st
-packetizerT _ _ st@LastForward{..} (_, bwdIn) = (nextStOut, (PacketStreamS2M False, Just _lastFragment))
+packetizerT2 toMetaOut _ Forward2{..} (Just inPkt, bwdIn) =
+  (nextStOut, (bwdIn, Just outPkt))
  where
-  nextStOut = if _ready bwdIn then def else st
-packetizerT _ _ s (Nothing, bwdIn) = (s, (bwdIn, Nothing))
+  nextAborted = _aborted2 || _abort inPkt
+  outPkt =
+    inPkt
+      { _meta = toMetaOut (_meta inPkt)
+      , _abort = nextAborted
+      }
+  nextStOut
+    | isJust (_last inPkt) && _ready bwdIn = LoadHeader2
+    | otherwise = Forward2 nextAborted
+packetizerT2 _ _ s (Nothing, bwdIn) = (s, (bwdIn, Nothing))
+
+data PacketizerState3 (headerBytes :: Nat) (dataWidth :: Nat)
+  = LoadHeader3
+  | Insert3
+      { _aborted3 :: Bool
+      , _hdrBuf3 :: Vec (headerBytes + dataWidth) (BitVector 8)
+      , _counter3 :: Index (headerBytes `DivRU` dataWidth)
+      }
+  | Forward3
+      { _aborted3 :: Bool
+      , _hdrBuf3 :: Vec (headerBytes + dataWidth) (BitVector 8)
+      }
+  | LastForward3
+      { _aborted3 :: Bool
+      , _hdrBuf3 :: Vec (headerBytes + dataWidth) (BitVector 8)
+      }
+  deriving (Generic, Show, ShowX)
+
+deriving instance
+  (KnownNat headerBytes, KnownNat dataWidth) =>
+  NFDataX (PacketizerState3 headerBytes dataWidth)
+
+-- | Packetizer transition function in case @dataWidth <= headerBytes@ and @headerBytes % dataWidth > 0@
+packetizerT3 ::
+  forall
+    (headerBytes :: Nat)
+    (dataWidth :: Nat)
+    (header :: Type)
+    (metaIn :: Type)
+    (metaOut :: Type).
+  (PacketizerCt header headerBytes dataWidth) =>
+  (1 <= headerBytes `Mod` dataWidth) =>
+  (headerBytes `Mod` dataWidth <= dataWidth) =>
+  (dataWidth <= headerBytes) =>
+  (metaIn -> metaOut) ->
+  (metaIn -> header) ->
+  PacketizerState3 headerBytes dataWidth ->
+  (Maybe (PacketStreamM2S dataWidth metaIn), PacketStreamS2M) ->
+  ( PacketizerState3 headerBytes dataWidth
+  , (PacketStreamS2M, Maybe (PacketStreamM2S dataWidth metaOut))
+  )
+packetizerT3 _ toHeader LoadHeader3 (Just inPkt, _bwdIn) =
+  (nextStOut, (PacketStreamS2M False, Nothing))
+ where
+  nextStOut = Insert3 False (bitCoerce (toHeader (_meta inPkt)) ++ _data inPkt) 0
+packetizerT3 toMetaOut _ st@Insert3{..} (Just inPkt, bwdIn) =
+  (nextStOut, (bwdOut, Just outPkt))
+ where
+  nextAborted = _aborted3 || _abort inPkt
+  (newHdrBuf, dataOut') = shiftOutFrom0 (SNat @dataWidth) _hdrBuf3
+
+  outPkt =
+    inPkt
+      { _data = dataOut'
+      , _last = lastOut
+      , _meta = toMetaOut (_meta inPkt)
+      }
+
+  (lastOut, nextSt) = case (_counter3 == maxBound, _last inPkt) of
+    (False, _) -> (Nothing, Insert3 nextAborted newHdrBuf (succ _counter3))
+    (True, Nothing) -> (Nothing, Forward3 nextAborted newHdrBuf)
+    (True, Just i) ->
+      if i < natToNum @(dataWidth - headerBytes `Mod` dataWidth)
+        then (Just (i + natToNum @(headerBytes `Mod` dataWidth)), LoadHeader3)
+        else (Nothing, LastForward3 nextAborted newHdrBuf)
+  nextStOut = if _ready bwdIn then nextSt else st
+  bwdOut = case nextSt of
+    LoadHeader3 -> bwdIn
+    Forward3{} -> bwdIn
+    _ -> PacketStreamS2M False
+packetizerT3 toMetaOut _ st@Forward3{..} (Just inPkt, bwdIn) =
+  (nextStOut, (bwdOut, Just outPkt))
+ where
+  bytesOut :: Vec (dataWidth - headerBytes `Mod` dataWidth) (BitVector 8)
+  buf :: Vec (headerBytes `Mod` dataWidth) (BitVector 8)
+  (bytesOut, buf) = splitAt (SNat @(dataWidth - headerBytes `Mod` dataWidth)) (_data inPkt)
+  newBuf :: Vec (headerBytes + dataWidth) (BitVector 8)
+  newBuf = buf ++ repeat @(headerBytes + dataWidth - headerBytes `Mod` dataWidth) defaultByte
+  nextAborted = _aborted3 || _abort inPkt
+
+  outPkt =
+    inPkt
+      { _data = take (SNat @(headerBytes `Mod` dataWidth)) _hdrBuf3 ++ bytesOut
+      , _last = lastOut
+      , _meta = toMetaOut (_meta inPkt)
+      , _abort = nextAborted
+      }
+
+  (lastOut, nextSt) = case _last inPkt of
+    Nothing -> (Nothing, Forward3 nextAborted newBuf)
+    Just i ->
+      if i < natToNum @(dataWidth - headerBytes `Mod` dataWidth)
+        then (Just (i + natToNum @(headerBytes `Mod` dataWidth)), LoadHeader3)
+        else (Nothing, LastForward3 nextAborted newBuf)
+  nextStOut = if _ready bwdIn then nextSt else st
+
+  bwdOut = case nextSt of
+    LastForward3{} -> PacketStreamS2M False
+    _ -> bwdIn
+packetizerT3 toMetaOut _ st@LastForward3{..} (Just inPkt, bwdIn) =
+  (nextStOut, (bwdIn, Just outPkt))
+ where
+  outPkt =
+    inPkt
+      { _data = takeLe (SNat @dataWidth) _hdrBuf3
+      , _last = (\i -> i - natToNum @(dataWidth - headerBytes `Mod` dataWidth)) <$> _last inPkt
+      , _meta = toMetaOut (_meta inPkt)
+      , _abort = _aborted3 || _abort inPkt
+      }
+  nextStOut = if _ready bwdIn then LoadHeader3 else st
+packetizerT3 _ _ s (Nothing, bwdIn) = (s, (bwdIn, Nothing))
 
 {- | Puts a portion of the metadata in front of the packet stream, and shifts the stream accordingly.
   This portion is defined by the metadata to header transformer function. If this function is `id`,
@@ -179,13 +297,15 @@ packetizerC ::
     (metaOut :: Type)
     (header :: Type)
     (headerBytes :: Nat).
-  (HiddenClockResetEnable dom) =>
-  (NFDataX metaOut) =>
-  (BitPack header) =>
-  (BitSize header ~ headerBytes * 8) =>
-  (KnownNat headerBytes) =>
-  (1 <= dataWidth) =>
-  (KnownNat dataWidth) =>
+  ( HiddenClockResetEnable dom
+  , NFDataX metaOut
+  , BitPack header
+  , BitSize header ~ headerBytes * 8
+  , KnownNat headerBytes
+  , 1 <= dataWidth
+  , 1 <= headerBytes
+  , KnownNat dataWidth
+  ) =>
   -- | Metadata transformer function
   (metaIn -> metaOut) ->
   -- | metaData to header that will be packetized transformer function
@@ -193,11 +313,19 @@ packetizerC ::
   Circuit (PacketStream dom dataWidth metaIn) (PacketStream dom dataWidth metaOut)
 packetizerC toMetaOut toHeader = fromSignals outCircuit
  where
-  outCircuit = case compareSNat (SNat @(ForwardBufSize headerBytes dataWidth)) (SNat @dataWidth) of
-    SNatLE -> mealyB (packetizerT @headerBytes toMetaOut toHeader) def
+  outCircuit = case compareSNat (SNat @(headerBytes `Mod` dataWidth)) (SNat @dataWidth) of
+    SNatLE -> case compareSNat (SNat @(headerBytes + 1)) (SNat @dataWidth) of
+      SNatLE -> mealyB (packetizerT1 @headerBytes toMetaOut toHeader) (Insert1 False)
+      SNatGT -> case sameNat (SNat @(headerBytes `Mod` dataWidth)) d0 of
+        Just Refl -> mealyB (packetizerT2 @headerBytes toMetaOut toHeader) LoadHeader2
+        _ -> case compareSNat d1 (SNat @(headerBytes `Mod` dataWidth)) of
+          SNatLE -> mealyB (packetizerT3 @headerBytes toMetaOut toHeader) LoadHeader3
+          SNatGT ->
+            clashCompileError
+              "packetizerC0: Absurd, Report this to the Clash compiler team: https://github.com/clash-lang/clash-compiler/issues"
     _ ->
       clashCompileError
-        "packetizerC: Absurd, Report this to the Clash compiler team: https://github.com/clash-lang/clash-compiler/issues"
+        "packetizerC1: Absurd, Report this to the Clash compiler team: https://github.com/clash-lang/clash-compiler/issues"
 
 data DfPacketizerState (metaOut :: Type) (headerBytes :: Nat) (dataWidth :: Nat)
   = DfIdle
