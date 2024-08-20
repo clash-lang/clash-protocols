89236539b5a90b01fdd6d3d54c69d2ea87a6fe0d
t-wallet <tw.personal@tutanota.com>
2024-08-20 12:16:45 +0200

    Add haxiom module

--- clash-protocols/tests/unittests.hs
+++ clash-protocols/tests/unittests.hs
@@ -1,12 +1,12 @@
 module Main where
 
 import Control.Concurrent (setNumCapabilities)
-import Control.Monad (join)
 import System.Environment (lookupEnv, setEnv)
 import Test.Tasty
 import Text.Read (readMaybe)
 import Prelude
 
+import qualified Tests.Haxioms
 import qualified Tests.Protocols
 
 main :: IO ()
@@ -15,7 +15,7 @@ main = do
   setEnv "TASTY_NUM_THREADS" "2"
 
   -- Detect "THREADS" environment variable on CI
-  nThreads <- join . fmap readMaybe <$> lookupEnv "THREADS"
+  nThreads <- (readMaybe =<<) <$> lookupEnv "THREADS"
   case nThreads of
     Nothing -> pure ()
     Just n -> do
@@ -27,5 +27,6 @@ tests :: TestTree
 tests =
   testGroup
     "Tests"
-    [ Tests.Protocols.tests
+    [ Tests.Haxioms.tests
+    , Tests.Protocols.tests
     ]
