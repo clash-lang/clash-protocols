d59432f2539e71c9b1c60ff635696bbefb191dcb
Lucas Bollen <lucas@qbaylogic.com>
2024-09-24 11:40:49 +0200

    Add clarification to `vecCircuits` that the inverse can not exist

--- clash-protocols/src/Protocols/Vec.hs
+++ clash-protocols/src/Protocols/Vec.hs
@@ -11,6 +11,9 @@ import Protocols.Plugin
 
 {- | "Bundle" together a 'C.Vec' of 'Circuit's into a 'Circuit' with 'C.Vec' input and output.
 The 'Circuit's all run in parallel.
+
+The inverse of 'vecCircuits' can not exist, as we can not guarantee that that the @n@th
+manager interface only depends on the @n@th subordinate interface.
 -}
 vecCircuits :: (C.KnownNat n) => C.Vec n (Circuit a b) -> Circuit (C.Vec n a) (C.Vec n b)
 vecCircuits fs = Circuit (\inps -> C.unzip $ f <$> fs <*> uncurry C.zip inps)
