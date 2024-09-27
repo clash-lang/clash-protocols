ee8bff29f8bc30497ee7223ae654efe39e879427
Lucas Bollen <lucas@qbaylogic.com>
2024-09-27 14:52:38 +0200

    Move `IdleCircuit` to `clash-protocols` and move Template haskell code to separate modules

--- clash-protocols/src/Protocols/Internal/TH.hs
+++ clash-protocols/src/Protocols/Internal/TH.hs
@@ -7,36 +7,6 @@ import Language.Haskell.TH
 
 import Protocols.Internal.Types
 
-appTs :: Q Type -> [Q Type] -> Q Type
-appTs = foldl appT
-
-protocolTupleInstances :: Int -> Int -> Q [Dec]
-protocolTupleInstances n m = mapM protocolTupleInstance [n .. m]
-
-protocolTupleInstance :: Int -> Q Dec
-protocolTupleInstance n =
-  instanceD
-    (pure []) -- context
-    (protocolConT `appT` tup) -- head
-    [mkTyInst fwdConName, mkTyInst bwdConName] -- body
- where
-  fwdConName = mkName "Fwd"
-  bwdConName = mkName "Bwd"
-  protocolConT = conT (mkName "Protocol")
-
-  tyVars :: [TypeQ]
-  tyVars = map (varT . mkName . ('a' :) . show) [1 .. n]
-
-  tup = tupleT n `appTs` tyVars
-
-  mkTyInst :: Name -> DecQ
-  mkTyInst con =
-    tySynInstD $ tySynEqn Nothing lhs rhs
-   where
-    lhs, rhs :: TypeQ
-    lhs = conT con `appT` tup
-    rhs = tupleT n `appTs` map (conT con `appT`) tyVars
-
 {- | Template haskell function to generate IdleCircuit instances for the tuples
 n through m inclusive. To see a 2-tuple version of the pattern we generate,
 see @Protocols.IdleCircuit@.
