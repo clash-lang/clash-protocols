399ae170be7149ca37c3dea77082b9065954cf16
t-wallet <tw.personal@tutanota.com>
2024-10-25 14:04:13 +0200

    Fix PacketFifo bug
    Now correctly drops packets that are too big to fit in the content FIFO in 'Backpressure' mode.

--- clash-protocols/tests/Tests/Protocols/PacketStream/PacketFifo.hs
+++ clash-protocols/tests/Tests/Protocols/PacketStream/PacketFifo.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 
 module Tests.Protocols.PacketStream.PacketFifo (
   tests,
@@ -14,6 +15,8 @@ import Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 
+import qualified Prelude as P
+
 import Test.Tasty
 import Test.Tasty.Hedgehog (HedgehogTestLimit (HedgehogTestLimit))
 import Test.Tasty.Hedgehog.Extra (testProperty)
@@ -25,44 +28,87 @@ import Protocols.PacketStream.Base
 import Protocols.PacketStream.Hedgehog
 import Protocols.PacketStream.PacketFifo
 
--- | test for id and proper dropping of aborted packets
-prop_packetFifo_id :: Property
-prop_packetFifo_id =
-  idWithModelSingleDomain
-    @System
-    defExpectOptions{eoSampleMax = 1000, eoStopAfterEmpty = 1000}
-    (genPackets 1 30 (genValidPacket defPacketOptions Gen.enumBounded (Range.linear 0 10)))
-    (exposeClockResetEnable dropAbortedPackets)
-    (exposeClockResetEnable ckt)
- where
-  ckt ::
-    (HiddenClockResetEnable System) =>
-    Circuit (PacketStream System 4 Int16) (PacketStream System 4 Int16)
-  ckt = packetFifoC d12 d12 Backpressure
+-- | Drops packets that consist of more than 2^n transfers.
+dropBigPackets ::
+  SNat n ->
+  [PacketStreamM2S dataWidth meta] ->
+  [PacketStreamM2S dataWidth meta]
+dropBigPackets n packets =
+  L.concat
+    $ L.filter
+      (\p -> L.length p < 2 P.^ snatToInteger n)
+      (chunkByPacket packets)
 
--- | test for id with a small buffer to ensure backpressure is tested
-prop_packetFifo_small_buffer_id :: Property
-prop_packetFifo_small_buffer_id =
+-- | Test for id and proper dropping of aborted packets.
+prop_packet_fifo_id :: Property
+prop_packet_fifo_id =
   idWithModelSingleDomain
     @System
-    defExpectOptions{eoSampleMax = 1000, eoStopAfterEmpty = 1000}
-    (genPackets 1 10 (genValidPacket defPacketOptions Gen.enumBounded (Range.linear 0 30)))
+    defExpectOptions
+    (genPackets 1 10 (genValidPacket defPacketOptions Gen.enumBounded (Range.linear 0 10)))
     (exposeClockResetEnable dropAbortedPackets)
-    (exposeClockResetEnable ckt)
+    (exposeClockResetEnable (packetFifoC @_ @1 @Int16 d10 d10 Backpressure))
+
+{- |
+Ensure that backpressure becayse of a full content RAM and dropping of packets
+that are too big to fit in the FIFO is tested.
+-}
+prop_packet_fifo_small_buffer_id :: Property
+prop_packet_fifo_small_buffer_id =
+  idWithModelSingleDomain
+    @System
+    defExpectOptions{eoStopAfterEmpty = 1000}
+    (genPackets 1 10 (genValidPacket defPacketOptions Gen.enumBounded (Range.linear 0 30)))
+    (exposeClockResetEnable (dropBigPackets d3 . dropAbortedPackets))
+    (exposeClockResetEnable (packetFifoC @_ @1 @Int16 d3 d5 Backpressure))
+
+-- | test for id using a small metabuffer to ensure backpressure using the metabuffer is tested
+prop_packet_fifo_small_meta_buffer_id :: Property
+prop_packet_fifo_small_meta_buffer_id =
+  idWithModelSingleDomain
+    @System
+    defExpectOptions
+    (genPackets 1 30 (genValidPacket defPacketOptions Gen.enumBounded (Range.linear 0 4)))
+    (exposeClockResetEnable dropAbortedPackets)
+    (exposeClockResetEnable (packetFifoC @_ @1 @Int16 d10 d2 Backpressure))
+
+-- | test for id and proper dropping of aborted packets
+prop_overFlowDrop_packetFifo_id :: Property
+prop_overFlowDrop_packetFifo_id =
+  idWithModelSingleDomain
+    @System
+    defExpectOptions
+    (genPackets 1 10 (genValidPacket defPacketOptions Gen.enumBounded (Range.linear 0 10)))
+    (exposeClockResetEnable dropAbortedPackets)
+    (exposeClockResetEnable (packetFifoC @_ @1 @Int16 d10 d10 Drop))
+
+-- | test for proper dropping when full
+prop_overFlowDrop_packetFifo_drop :: Property
+prop_overFlowDrop_packetFifo_drop =
+  propWithModelSingleDomain
+    @System
+    defExpectOptions
+    -- make sure the timeout is long as the packetFifo can be quiet for a while while dropping
+    (liftA3 (\a b c -> a L.++ b L.++ c) genSmall genBig genSmall)
+    (exposeClockResetEnable id)
+    (exposeClockResetEnable (packetFifoC @_ @4 @Int16 d3 d5 Drop))
+    (\xs ys -> diff ys L.isSubsequenceOf xs)
  where
-  ckt ::
-    (HiddenClockResetEnable System) =>
-    Circuit (PacketStream System 4 Int16) (PacketStream System 4 Int16)
-  ckt = packetFifoC d5 d5 Backpressure
+  genSmall =
+    genValidPacket defPacketOptions{poAbortMode = NoAbort} Gen.enumBounded (Range.linear 0 3)
+  genBig =
+    genValidPacket
+      defPacketOptions{poAbortMode = NoAbort}
+      Gen.enumBounded
+      (Range.linear 9 9)
 
 -- | test to check if there are no gaps inside of packets
 prop_packetFifo_no_gaps :: Property
 prop_packetFifo_no_gaps = property $ do
-  let packetFifoSize = d12
-      maxInputSize = 50
+  let maxInputSize = 50
       ckt =
         exposeClockResetEnable
-          (packetFifoC packetFifoSize packetFifoSize Backpressure)
+          (packetFifoC d12 d12 Backpressure)
           systemClockGen
           resetGen
           enableGen
@@ -75,7 +121,7 @@ prop_packetFifo_no_gaps = property $ do
 
   packets :: [PacketStreamM2S 4 Int16] <- forAll gen
 
-  let packetSize = 2 Prelude.^ snatToInteger packetFifoSize
+  let packetSize = 2 P.^ snatToInteger d12
       cfg = SimulationConfig 1 (2 * packetSize) False
       cktResult = simulateC ckt cfg (Just <$> packets)
 
@@ -86,68 +132,9 @@ prop_packetFifo_no_gaps = property $ do
   noGaps (_ : xs) = noGaps xs
   noGaps _ = True
 
--- | test for id and proper dropping of aborted packets
-prop_overFlowDrop_packetFifo_id :: Property
-prop_overFlowDrop_packetFifo_id =
-  idWithModelSingleDomain
-    @System
-    defExpectOptions{eoSampleMax = 2000, eoStopAfterEmpty = 1000}
-    (genPackets 1 30 (genValidPacket defPacketOptions Gen.enumBounded (Range.linear 0 10)))
-    (exposeClockResetEnable dropAbortedPackets)
-    (exposeClockResetEnable ckt)
- where
-  ckt ::
-    (HiddenClockResetEnable System) =>
-    Circuit (PacketStream System 4 Int16) (PacketStream System 4 Int16)
-  ckt = packetFifoC d12 d12 Drop
-
--- | test for proper dropping when full
-prop_overFlowDrop_packetFifo_drop :: Property
-prop_overFlowDrop_packetFifo_drop =
-  idWithModelSingleDomain
-    @System
-    defExpectOptions{eoSampleMax = 1000, eoStopAfterEmpty = 1000}
-    -- make sure the timeout is long as the packetFifo can be quiet for a while while dropping
-    (liftA3 (\a b c -> a L.++ b L.++ c) genSmall genBig genSmall)
-    (exposeClockResetEnable model)
-    (exposeClockResetEnable ckt)
- where
-  ckt ::
-    (HiddenClockResetEnable System) =>
-    Circuit (PacketStream System 4 Int16) (PacketStream System 4 Int16)
-  ckt = packetFifoC d5 d5 Drop
-
-  model :: [PacketStreamM2S 4 Int16] -> [PacketStreamM2S 4 Int16]
-  model packets = Prelude.concat $ L.take 1 packetChunk L.++ L.drop 2 packetChunk
-   where
-    packetChunk = chunkByPacket packets
-
-  genSmall =
-    genValidPacket defPacketOptions{poAbortMode = NoAbort} Gen.enumBounded (Range.linear 1 5)
-  genBig =
-    genValidPacket
-      defPacketOptions{poAbortMode = NoAbort}
-      Gen.enumBounded
-      (Range.linear 33 33)
-
--- | test for id using a small metabuffer to ensure backpressure using the metabuffer is tested
-prop_packetFifo_small_metaBuffer :: Property
-prop_packetFifo_small_metaBuffer =
-  idWithModelSingleDomain
-    @System
-    defExpectOptions
-    (genPackets 1 30 (genValidPacket defPacketOptions Gen.enumBounded (Range.linear 0 4)))
-    (exposeClockResetEnable dropAbortedPackets)
-    (exposeClockResetEnable ckt)
- where
-  ckt ::
-    (HiddenClockResetEnable System) =>
-    Circuit (PacketStream System 4 Int16) (PacketStream System 4 Int16)
-  ckt = packetFifoC d12 d2 Backpressure
-
 tests :: TestTree
 tests =
-  localOption (mkTimeout 20_000_000 {- 20 seconds -}) $
-    localOption
-      (HedgehogTestLimit (Just 100))
+  localOption (mkTimeout 20_000_000 {- 20 seconds -})
+    $ localOption
+      (HedgehogTestLimit (Just 500))
       $(testGroupGenerator)
