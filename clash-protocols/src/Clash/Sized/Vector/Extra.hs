9120a9b127076fc78c6a3f45fd2ce4c387a0da8c
t-wallet <tw.personal@tutanota.com>
2024-10-28 09:21:35 +0100

    Remove global constraint solver iterations and unused code

--- clash-protocols/src/Clash/Sized/Vector/Extra.hs
+++ clash-protocols/src/Clash/Sized/Vector/Extra.hs
@@ -3,7 +3,6 @@
 module Clash.Sized.Vector.Extra (
   dropLe,
   takeLe,
-  appendVec,
 ) where
 
 import Clash.Prelude
@@ -35,25 +34,3 @@ takeLe ::
   Vec m a ->
   Vec n a
 takeLe SNat vs = leToPlus @n @m $ takeI vs
-
--- | Take the first @valid@ elements of @xs@, append @ys@, then pad with 0s
-appendVec ::
-  forall n m a.
-  (KnownNat n) =>
-  (Num a) =>
-  Index n ->
-  Vec n a ->
-  Vec m a ->
-  Vec (n + m) a
-appendVec valid xs ys = results !! valid
- where
-  go :: forall l. SNat l -> Vec (n + m) a
-  go l@SNat =
-    let f = addSNat l d1
-     in case compareSNat f (SNat @n) of
-          SNatLE -> takeLe (addSNat l d1) xs ++ ys ++ extra
-           where
-            extra :: Vec (n - (l + 1)) a
-            extra = repeat 0
-          _ -> error "appendVec: Absurd"
-  results = smap (\s _ -> go s) xs
